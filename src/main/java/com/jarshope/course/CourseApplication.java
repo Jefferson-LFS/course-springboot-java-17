5816c189eefdf9bfe30257cd6ab8e0e8a73cf221
Jefferson Lucas <54781630+Jefferson-LFS@users.noreply.github.com>
2022-09-02 00:31:40 -0300

    H2 database, test profile, JPA

--- src/main/java/com/jarshope/course/CourseApplication.java
+++ src/main/java/com/jarshope/course/CourseApplication.java
@@ -2,9 +2,8 @@ package com.jarshope.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class CourseApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/jarshope/course/domain/model/User.java
+++ src/main/java/com/jarshope/course/domain/model/User.java
@@ -3,6 +3,12 @@ package com.jarshope.course.domain.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -12,11 +18,15 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
 	
   private static final long serialVersionUID = 1L;
-
+  
   @EqualsAndHashCode.Include
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   
   private String name;
